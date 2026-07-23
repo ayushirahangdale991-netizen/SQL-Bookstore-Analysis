c6560a0b92e090ab0a1095d005f7e8de431075f9
Ayushi Rahangdale <ayushirahangdale991@gmail.com>
2026-07-23 12:54:44 +0530

    Update bookstore analysis.sql

--- bookstore analysis.sql
+++ bookstore analysis.sql
@@ -67,11 +67,9 @@ SELECT * FROM Orders
 WHERE quantity>1;
 
 
-
 -- 8) Retrieve all orders where the total amount exceeds $20:
 SELECT * FROM Orders 
 WHERE total_amount>20;
-
 
 
 -- 9) List all genres available in the Books table:
@@ -122,26 +120,26 @@ GROUP BY o.book_id, b.title
 ORDER BY ORDER_COUNT DESC LIMIT 1;
 
 
--- 4) Find the most frequently ordered book:
+-- 5) Find the most frequently ordered book:
 SELECT o.Book_id, b.title, COUNT(o.order_id) AS ORDER_COUNT
 FROM orders o
 JOIN books b ON o.book_id=b.book_id
 GROUP BY o.book_id, b.title
 ORDER BY ORDER_COUNT DESC LIMIT 1;
 
--- 5) Show the top 3 most expensive books of 'Fantasy' Genre :
+-- 6) Show the top 3 most expensive books of 'Fantasy' Genre :
 SELECT * FROM books
 WHERE genre ='Fantasy'
 ORDER BY price DESC LIMIT 3;
 
--- 6) Retrieve the total quantity of books sold by each author:
+-- 7) Retrieve the total quantity of books sold by each author:
 
 SELECT b.author, SUM(o.quantity) AS Total_Books_Sold
 FROM orders o
 JOIN books b ON o.book_id=b.book_id
 GROUP BY b.Author;
 
--- 7) List the cities where customers who spent over $30 are located:
+-- 8) List the cities where customers who spent over $30 are located:
 
 SELECT DISTINCT c.city, total_amount
 FROM orders o
@@ -149,14 +147,14 @@ JOIN customers c ON o.customer_id=c.customer_id
 WHERE o.total_amount > 30;
 
 
--- 8) Find the customer who spent the most on orders:
+-- 9) Find the customer who spent the most on orders:
 SELECT c.customer_id, c.name, SUM(o.total_amount) AS Total_Spent
 FROM orders o
 JOIN customers c ON o.customer_id=c.customer_id
 GROUP BY c.customer_id, c.name
 ORDER BY Total_spent Desc LIMIT 1;
 
--- 9) Calculate the stock remaining after fulfilling all orders:
+-- 10) Calculate the stock remaining after fulfilling all orders:
 
 SELECT b.book_id, b.title, b.stock, COALESCE(SUM(o.quantity),0) AS Order_quantity,  
 	b.stock- COALESCE(SUM(o.quantity),0) AS Remaining_Quantity
