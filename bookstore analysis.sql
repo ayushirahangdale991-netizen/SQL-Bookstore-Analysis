5fbe3da03badb4732d46a3fcfbde98ca51d50753
Ayushi Rahangdale <ayushirahangdale991@gmail.com>
2026-07-23 13:47:35 +0530

    Update bookstore analysis.sql

--- bookstore analysis.sql
+++ bookstore analysis.sql
@@ -67,9 +67,11 @@ SELECT * FROM Orders
 WHERE quantity>1;
 
 
+
 -- 8) Retrieve all orders where the total amount exceeds $20:
 SELECT * FROM Orders 
 WHERE total_amount>20;
+
 
 
 -- 9) List all genres available in the Books table:
@@ -89,28 +91,26 @@ FROM Orders;
 
 -- Advanced Analytical Queries  
 
--- 1) Retrieve the total number of books sold for each genre:
-
-SELECT * FROM ORDERS;
-
-SELECT b.Genre, SUM(o.Quantity) AS Total_Books_sold
-FROM Orders o
-JOIN Books b ON o.book_id = b.book_id
-GROUP BY b.Genre;
-
-
--- 2) Find the average price of books in the "Fantasy" genre:
+-- 1) Find the average price of books in the "Fantasy" genre:
 SELECT AVG(price) AS Average_Price
 FROM Books
 WHERE Genre = 'Fantasy';
 
 
--- 3) List customers who have placed at least 2 orders:
+-- 2) List customers who have placed at least 2 orders:
 SELECT o.customer_id, c.name, COUNT(o.Order_id) AS ORDER_COUNT
 FROM orders o
 JOIN customers c ON o.customer_id=c.customer_id
 GROUP BY o.customer_id, c.name
 HAVING COUNT(Order_id) >=2;
+
+-- 3) Find the most frequently ordered book:
+SELECT o.Book_id, b.title, COUNT(o.order_id) AS ORDER_COUNT
+FROM orders o
+JOIN books b ON o.book_id=b.book_id
+GROUP BY o.book_id, b.title
+ORDER BY ORDER_COUNT DESC LIMIT 1;
+
 
 -- 4) Find the most frequently ordered book:
 SELECT o.Book_id, b.title, COUNT(o.order_id) AS ORDER_COUNT
@@ -119,27 +119,19 @@ JOIN books b ON o.book_id=b.book_id
 GROUP BY o.book_id, b.title
 ORDER BY ORDER_COUNT DESC LIMIT 1;
 
-
--- 5) Find the most frequently ordered book:
-SELECT o.Book_id, b.title, COUNT(o.order_id) AS ORDER_COUNT
-FROM orders o
-JOIN books b ON o.book_id=b.book_id
-GROUP BY o.book_id, b.title
-ORDER BY ORDER_COUNT DESC LIMIT 1;
-
--- 6) Show the top 3 most expensive books of 'Fantasy' Genre :
+-- 5) Show the top 3 most expensive books of 'Fantasy' Genre :
 SELECT * FROM books
 WHERE genre ='Fantasy'
 ORDER BY price DESC LIMIT 3;
 
--- 7) Retrieve the total quantity of books sold by each author:
+-- 6) Retrieve the total quantity of books sold by each author:
 
 SELECT b.author, SUM(o.quantity) AS Total_Books_Sold
 FROM orders o
 JOIN books b ON o.book_id=b.book_id
 GROUP BY b.Author;
 
--- 8) List the cities where customers who spent over $30 are located:
+-- 7) List the cities where customers who spent over $30 are located:
 
 SELECT DISTINCT c.city, total_amount
 FROM orders o
@@ -147,19 +139,58 @@ JOIN customers c ON o.customer_id=c.customer_id
 WHERE o.total_amount > 30;
 
 
--- 9) Find the customer who spent the most on orders:
+-- 8) Find the customer who spent the most on orders:
 SELECT c.customer_id, c.name, SUM(o.total_amount) AS Total_Spent
 FROM orders o
 JOIN customers c ON o.customer_id=c.customer_id
 GROUP BY c.customer_id, c.name
 ORDER BY Total_spent Desc LIMIT 1;
 
--- 10) Calculate the stock remaining after fulfilling all orders:
+-- 9) Calculate the stock remaining after fulfilling all orders:
 
 SELECT b.book_id, b.title, b.stock, COALESCE(SUM(o.quantity),0) AS Order_quantity,  
 	b.stock- COALESCE(SUM(o.quantity),0) AS Remaining_Quantity
 FROM books b
 LEFT JOIN orders o ON b.book_id=o.book_id
 GROUP BY b.book_id ORDER BY b.book_id;
+
+-- 10) Identify Top-Selling Books
+SELECT
+    b.Book_ID,
+    b.Title,
+    SUM(o.Quantity) AS Total_Copies_Sold
+FROM Orders o
+JOIN Books b
+ON o.Book_ID = b.Book_ID
+GROUP BY b.Book_ID, b.Title
+ORDER BY Total_Copies_Sold DESC
+LIMIT 10;
+
+-- 11) Most Frequently Ordered Books
+SELECT
+    b.Title,
+    COUNT(o.Order_ID) AS Times_Ordered
+FROM Orders o
+JOIN Books b
+ON o.Book_ID = b.Book_ID
+GROUP BY b.Title
+ORDER BY Times_Ordered DESC
+LIMIT 10;
+
+-- 12)Revenue Generated
+SELECT
+    SUM(Total_Amount) AS Total_Revenue
+FROM Orders;
+
+-- 13)Monthly Revenue Trend
+SELECT
+    DATE_FORMAT(Order_Date,'%Y-%m') AS Month,
+    SUM(Total_Amount) AS Revenue
+FROM Orders
+GROUP BY Month
+ORDER BY Month;
+
+
+
 
 
